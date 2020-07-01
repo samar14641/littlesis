e294198f4b90627bd4b765f81b2347dbcfe14c39
Samar Dikshit <samard05@gmail.com>
2020-06-30 22:13:09 -0400

    collector() updates

--- collection.py
+++ collection.py
@@ -4,6 +4,7 @@ import pickle
 import requests
 import time
 from pprint import pprint
+from sys import exit
 
 minSleep = 0.5
 
@@ -48,8 +49,16 @@ def simpleIterativeCollect():
     with open(os.getcwd() + '\\Data\\collect150.json', 'w', encoding = 'utf-8')  as f:
         json.dump(data, f, ensure_ascii = False, indent = 4)
 
+def collectorInit():
+    entID = 1
+
+    with open(os.getcwd() + '\\Pickle\\entID.pickle', 'wb') as pckl:
+        pickle.dump(entID, pckl, pickle.HIGHEST_PROTOCOL)
+
 def collector():
-    entID = 1  # entity ID
+    startTime = time.time()
+
+    entID = None  # entity ID
     count = 0
     maxCollect = 1000
     baseURL = 'https://littlesis.org/api/entities/'
@@ -60,6 +69,15 @@ def collector():
     notOk = set()
 
     currentFile = 1
+
+    with open(os.getcwd() + '\\Pickle\\entID.pickle', 'rb') as pckl:
+        entID = pickle.load(pckl)
+
+    if entID is None:
+        print('entity ID is None, aborting')
+        exit(1)
+
+    print('Starting from', entID)
 
     while count <= maxCollect - 1:
         try:
@@ -74,16 +92,16 @@ def collector():
 
             else: 
                 print(entID, 'NOT OK', resp.status_code)
-                notOk.add((entID, resp.status_code))
+                notOk.add((entID, resp.status_code))  # log entID and resp code
 
         except Exception as e:
             print(entID, e)
-            excpt.add(entID)
+            excpt.add(entID)  # log entID
             continue
 
         entID += 1
 
-        if count % 100 == 0 and data:
+        if count % 100 == 0 and data:  # write after every 100 entities
             with open(os.getcwd() + '\\Data\\entity' + str(currentFile) + '.json', 'w', encoding = 'utf-8')  as f:
                 json.dump(data, f, ensure_ascii = False, indent = 4)   
 
@@ -101,7 +119,14 @@ def collector():
     with open(os.getcwd() + '\\Pickle\\notOk.pickle', 'wb') as pckl:
         pickle.dump(notOk, pckl, pickle.HIGHEST_PROTOCOL)
 
+    # TO DO:
+    # -> data logger outside loop for final round of data
+    # -> notOk logger in loop if notOk is not {} every 1000 iters, use currentFile counter
+
+    endTime = time.time()
+    print('Time taken (min):', (endTime - startTime) / 60)
     print('Count:', count)
 
 # simpleIterativeCollect()
+# collectorInit()
 collector()
